28561029a3271443cc3db8d2634da4752f93312f
Dustin Hengel <djhyv2@mail.missouri.edu>
2020-07-22 22:33:29 -0500

    Attempt at using hooks to edit

--- Frontend/src/components/OrgChart.jsx
+++ Frontend/src/components/OrgChart.jsx
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-//import OrgChart from '@balkangraph/orgchart.js/orgchart';
-import OrgChart from '../orgchart';
+import OrgChart from '@balkangraph/orgchart.js/orgchart';
 import './OrgChart.css';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 
 class OrgChartComponent extends Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.editHandler = this.editHandler.bind(this);
+    }
+
     componentDidMount()
     {
         setTimeout(async () =>
         {
             const { nodes } = this.props;
+            console.log(this.props);
             OrgChart.templates.ana.field_0 = '<text class="field_0" style="font-size: 20px;" fill="#ffffff" x="150" y="30" text-anchor="middle">{val}</text>';
             OrgChart.templates.ana.field_1 = '<text class="field_1" style="font-size: 14px;" fill="#ffffff" x="150" y="50" text-anchor="middle">{val}</text>';
             OrgChart.templates.ana.field_2 = '<text class="field_2" style="font-size: 14px;" fill="#ffffff" x="150" y="70" text-anchor="middle">{val}</text>';
@@ -22,7 +29,7 @@ class OrgChartComponent extends Component
             {
                 tags[`subLevels${i}`] = { subLevels: i };
             }
-            const chart = new OrgChart(document.getElementById('orgchart'), {
+            this.chart = new OrgChart(document.getElementById('orgchart'), {
                 nodes,
                 nodeBinding: {
                     field_0: 'Name',
@@ -37,7 +44,11 @@ class OrgChartComponent extends Component
                     csv: { text: 'Export CSV' },
                 },
                 nodeMenu: {
-                    edit: { text: 'Edit' },
+                    call: {
+                        icon: '<svg width="24" height="24" viewBox="0 0 528.899 528.899"><path fill="#7A7A7A" d="M328.883,89.125l107.59,107.589l-272.34,272.34L56.604,361.465L328.883,89.125z M518.113,63.177l-47.981-47.981 c-18.543-18.543-48.653-18.543-67.259,0l-45.961,45.961l107.59,107.59l53.611-53.611 C532.495,100.753,532.495,77.559,518.113,63.177z M0.3,512.69c-1.958,8.812,5.998,16.708,14.811,14.565l119.891-29.069 L27.473,390.597L0.3,512.69z" /></svg>',
+                        text: 'Edit',
+                        onClick: this.editHandler,
+                    },
                 },
                 tags,
                 scaleInitial: OrgChart.match.boundary,
@@ -45,7 +56,7 @@ class OrgChartComponent extends Component
                 siblingSeparation: 15,
             });
 
-            chart.on('exportstart', (semder, args) =>
+            this.chart.on('exportstart', (semder, args) =>
             {
                 if ('csv' === args.ext)
                 {
@@ -64,38 +75,13 @@ class OrgChartComponent extends Component
                 }
             });
 
-            chart.editUI.on('field', (sender, args) =>
+            this.chart.editUI.on('field', (sender, args) =>
             {
                 if (['Add new field', 'isAssistant', 'bigBrotherYear'].includes(args.name))
                 {
                     return false;
                 }
-
-                if ('Big Brother' == args.name)
-                {
-                    const txt = args.field.querySelector('input');
-                    const txtVal = txt.value;
-                    if (txt)
-                    {
-                        txt.style.color = 'red';
-
-                        const select = document.createElement('select');
-                        select.innerHTML = '<option value="CEO">CEO</option>'
-                        + '<option value="IT Manager">IT Manager</option>'
-                        + '<option value="HR Manager">HR Manager</option>';
-
-                        select.style.width = '100%';
-                        select.setAttribute('val', '');
-                        select.style.fontSize = '16px';
-                        select.style.color = 'rgb(122, 122, 122)';
-                        select.style.paddingTop = '7px';
-                        select.style.paddingBottom = '7px';
-                        select.value = txtVal;
-
-                        txt.parentNode.appendChild(select);
-                        txt.parentNode.removeChild(txt);
-                    }
-                }
+                return true;
             });
         }, 1000);
     }
@@ -103,6 +89,15 @@ class OrgChartComponent extends Component
     shouldComponentUpdate()
     {
         return false;
+    }
+
+    editHandler(nodeId)
+    {
+        //const nodeData = this.chart.get(nodeId);
+        //this.props.history.push({ pathname: '/bylaws' });
+        console.log(this.props);
+        //useHistory().push('/bylaws');
+        window.location.href = '/Bylaws';
     }
 
     render()
